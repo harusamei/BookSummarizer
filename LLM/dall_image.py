e61e250ee97c7d5bac2dd87acfe7a17c8d55bbe0
harusamei <harusamei@126.com>
2025-04-25 11:18:06 +0800

    fix image命名及重复处理

--- LLM/dall_image.py
+++ LLM/dall_image.py
@@ -4,6 +4,7 @@ import http.client
 import json
 import requests
 import asyncio
+import time
 from PIL import Image, PngImagePlugin
 import os
 import sys
@@ -25,26 +26,30 @@ def makePayload(prompt):
       "prompt": prompt,
       "n": 1,
       "model": "dall-e-3",
-      "size": "512x512"
+      "size": "1024x1024"
    })
    return payload
 
-async def askDall(payload):
-   # 生成图像
-   conn = http.client.HTTPSConnection("api.chatanywhere.tech")
-   headers = {
-      'Authorization': f'Bearer {key}',
-      'Content-Type': 'application/json'
-   }
-   conn.request("POST", "/v1/images/generations", payload, headers)
-   res = conn.getresponse()
-   data = res.read()
-   print(data.decode("utf-8"))
-   response_json = json.loads(data.decode("utf-8"))
-   image_url = response_json['data'][0]['url']
-
-   print(f"生成的图像URL: {image_url}")
-   return image_url
+async def askDall(payload, retries=2, delay=2):
+   for attempt in range(retries):
+      try:
+         conn = http.client.HTTPSConnection("api.chatanywhere.tech")
+         headers = {
+               'Authorization': f'Bearer {key}',
+               'Content-Type': 'application/json'
+         }
+         conn.request("POST", "/v1/images/generations", payload, headers)
+         res = conn.getresponse()
+         data = res.read()
+         response_json = json.loads(data.decode("utf-8"))
+         image_url = response_json['data'][0]['url']
+         return image_url
+      except http.client.RemoteDisconnected as e:
+         print(f"Attempt {attempt + 1} failed: {e}")
+         if attempt < retries - 1:
+               time.sleep(delay)  # 等待一段时间后重试
+         else:
+               return None  # 所有重试都失败了
 
 async def download_image(image_url, output_path):
    # 下载图像
@@ -55,8 +60,14 @@ async def download_image(image_url, output_path):
 
 # 保存元信息只能使用png格式
 async def save_image_with_prompt(image_url, prompt, output_path='image.png'):
+   if image_url is None:
+      print("Image URL is None. Cannot download image.")
+      return False
    # 下载图像
    response = requests.get(image_url)
+   if response.status_code != 200:  # 检查状态码是否为 200（成功）
+      print(f"Failed to download image. HTTP status code: {response.status_code}")
+      return False
    img = Image.open(BytesIO(response.content))
 
    # 添加描述到元数据
@@ -65,6 +76,8 @@ async def save_image_with_prompt(image_url, prompt, output_path='image.png'):
 
    # 保存图像并附加元数据
    img.save(output_path, "PNG", pnginfo=metadata)
+   print(f"Image saved to {output_path} with prompt metadata.")
+   return True
 
 def view_image_metadata(image_file):
     """
--- summary.py
+++ summary.py
@@ -1,4 +1,4 @@
-# 制作书籍的简介和视频脚本
+# 生成用于文字版和视频版的文档
 ####################################################
 import sys
 import os
@@ -24,22 +24,23 @@ class BookSummary:
         # 文件使用的扩展名
         self.exts = {
             'bookInfo': '.json',
-            'introduction': '_std.md',  # 文字版导言
-            'videoScript': '_bv.md',    # B站风视频脚本
-            'visualDesc': '_desc.json',    # 图像，视频描述
-            'official': '_intro.txt',      # 官方或书籍序言
-            'videoStructure': '_vid.json',  # 视频结构
-            'reused': '_reuse.json'  # 重用的内容
+            'introduction': '_std.md',      # 文字版导言
+            'voiceover': '_bv.md',        # B站视频旁白
+            'visualDesc': '_desc.json',     # 图像，视频描述
+            'official': '_intro.txt',       # 官方或书籍序言
+            'videoStructure': '_vid.json'   # 视频结构
         }
+        self.flags = {}
 
     async def gen_batch(self, xls_file):
         books = pd.read_excel(xls_file, sheet_name=0)
         books.columns = books.columns.str.strip()  # 去除列名中的多余空格
-        books.fillna(inplace=True,value='')
+        books.fillna(inplace=True, value='')
         book_dir = os.path.dirname(os.path.abspath(xls_file))
         
         for _, row in books.iterrows():
             hasWritten, hasIntroduction = False, False
+            self.flags = {}
             title = row['title']
             if title.startswith('《'):
                 title = title[1:-1]
@@ -50,39 +51,44 @@ class BookSummary:
                 hasWritten = True
             if hasIntroduction.lower() == 'y':
                 hasIntroduction = True
-           
-            # print(f'author: {author}, title: {title}')
-            # await self.gen_bookInfo(title, author, book_dir)
-            # print('part 1: generate book summarization')
-            # # 有人工写的导读
-            # if hasWritten is True:
-            #     print(f"Book {title} already has a human-written.")
-            # elif hasIntroduction is True:   # 书籍序言或专业导读
-            #     print(f"Book {title} already has an official introduction .")
-            #     await self.rewrite_intro(title, author, book_dir)
-            # else:
-            #     await self.gen_introduction(title, author, book_dir)
+            # 创建以书名命名的目录
+            sub_book_dir = os.path.join(book_dir, title)
+            if not os.path.exists(sub_book_dir):
+                os.makedirs(sub_book_dir)
+
+            print(f'author: {author}, title: {title}')
+            
+            self.flags['bookInfo'] = await self.gen_bookInfo(title, author, sub_book_dir)
+
+            print('part 1: generate book summarization')
+            # 有人工写的导读
+            if hasWritten is True:
+                print(f"Book {title} already has a human-written.")
+            elif hasIntroduction is True:   # 书籍序言或专业导读
+                print(f"Book {title} already has an official introduction .")
+                self.flags['introduction'] = await self.rewrite_intro(title, author, sub_book_dir)
+            else:
+                self.flags['introduction'] = await self.gen_introduction(title, author, sub_book_dir)
         
             print('part 2: generate visual content')
-            # await self.gen_videoStructure(title, book_dir)
-            await self.gen_visualDesc(title, book_dir)
+            self.flags['videoStructure'] = await self.gen_videoStructure(title, sub_book_dir)
+            self.flags['visualDesc'] = await self.gen_visualDesc(title, sub_book_dir)
             
-            # print('part 3: generate voiceover script')
-            # await self.gen_videoScript(title, author, book_dir)
+            print('part 3: generate voiceover script')
+            self.flags['voiceover'] = await self.gen_voiceover(title, author, sub_book_dir)
             break
     
     # 输出json 存放在book_dir 目录下
     async def gen_bookInfo(self, title, author, book_dir):
 
         tmpls = {}
-        tasks1 = ['brief_intro', 'key_scenes']
-        print(tasks1)
-
-        for key in tasks1:
+        tasks = ['brief_intro', 'key_scenes']
+        for key in tasks:
             tmpls[key] = self.prompter.tasks[key]
 
         abook = {}
-        for key in tasks1:
+        for key in tasks:
+            print(f"gen {key} for {title}")
             query = tmpls[key].format(title=title, author=author, language=self.lang)
             asw = await self.llm.ask_llm(query, '')
             result = self.ans_extr.output_extr(key, asw)
@@ -101,54 +107,61 @@ class BookSummary:
         
         abook = {}
         tmpl = self.prompter.tasks['book_introduction']
-        ext = self.exts['bookInfo']
-        with open(os.path.join(book_dir, f'{title}{ext}'), 'r', encoding='utf-8') as f:
-            abook = json.load(f)
-        if len(abook.keys()) == 0:
+        if self.flags.get('bookInfo') is False:
             abook['author'] = author
             abook['title'] = title
+        else:
+            ext = self.exts['bookInfo']
+            with open(os.path.join(book_dir, f'{title}{ext}'), 'r', encoding='utf-8') as f:
+                abook = json.load(f)
+        
         jsonStr = json.dumps(abook, ensure_ascii=False, indent=4)
         query = tmpl.format(book_info=jsonStr)
         asw = await self.llm.ask_llm(query, '')
         ext = self.exts['introduction']
         self.dp.save_txt(os.path.join(book_dir, f'{title}{ext}'), asw)
 
-        return
+        return True
     
     async def rewrite_intro(self, title, author, book_dir):
         print(f"rewrite intro for {title}")
         tmpl = self.prompter.tasks['intro_rewrite']
-        ext = self.exts['bookInfo']
-        with open(os.path.join(book_dir, f'{title}{ext}'), 'r', encoding='utf-8') as f:
-            abook = json.load(f)
-        if len(abook.keys()) == 0:
+        if self.flags.get('bookInfo') is True:
+            ext = self.exts['bookInfo']
+            with open(os.path.join(book_dir, f'{title}{ext}'), 'r', encoding='utf-8') as f:
+                abook = json.load(f)
+        else:
             abook = {}
         bookInfo = json.dumps(abook, ensure_ascii=False, indent=4)
 
         ext = self.exts['official']
-        with open(os.path.join(book_dir, f'{title}{ext}'), 'r', encoding='utf-8') as f:
-            oguide = f.read()
-        if len(oguide) == 0:
-            print(f"Failed to read {title}{ext} in rewrite_intro.")
+        filePath = os.path.join(book_dir, f'{title}{ext}')
+        if os.path.exists(filePath):
+            with open(filePath, 'r', encoding='utf-8') as f:
+                oguide = f.read()
+        else:
+            print(f"Failed to load {title}{ext} in rewrite_intro.")
             return False
+        
         query = tmpl.format(book_info=bookInfo, intro_txt=oguide)
-        # with open('temp_query.txt', 'w', encoding='utf-8') as f:
-        #     f.write(query)
-
+        
         asw = await self.llm.ask_llm(query, '')
         ext = self.exts['introduction']
         self.dp.save_txt(os.path.join(book_dir, f'{title}{ext}'), asw)
 
         return True
     
-    # B站视频脚本
-    async def gen_videoScript(self, title, author, book_dir):
+    # B站视频旁白
+    async def gen_voiceover(self, title, author, book_dir):
         
-        tmpl = self.prompter.tasks['b_video_script']
+        if self.flags['videoStructure'] is False or self.flags['introduction'] is False:
+            print(f"Failed to generate voicevoer for {title}")
+            return False
+        
+        tmpl = self.prompter.tasks['b_voiceover']
         ext = self.exts['videoStructure']
         with open(os.path.join(book_dir, f'{title}{ext}'), 'r', encoding='utf-8') as f:
             vid = json.load(f)
-        
         vid_stru = json.dumps(vid, ensure_ascii=False, indent=4)
         ext = self.exts['introduction']
         with open(os.path.join(book_dir, f'{title}{ext}'), 'r', encoding='utf-8') as f:
@@ -156,28 +169,31 @@ class BookSummary:
         query = tmpl.format(vid_stru=vid_stru, std_txt=intro_txt)
         asw = await self.llm.ask_llm(query, '')
 
-        ext = self.exts['videoScript']
+        ext = self.exts['voiceover']
         self.dp.save_txt(os.path.join(book_dir, f'{title}{ext}'), asw)
+        return True
         
-    
     # 基于文字版intro+bookinfo 生成视频结构
     async def gen_videoStructure(self, title, book_dir):
         print(f"gen_videoStructure for {title}")
 
         tmpl = self.prompter.tasks['video_structure1']
-        ext = self.exts['bookInfo']
-        with open(os.path.join(book_dir, f'{title}{ext}'), 'r', encoding='utf-8') as f:
-            abook = json.load(f)
-        if len(abook.keys()) == 0:
+        if self.flags.get('bookInfo') is True:
+            ext = self.exts['bookInfo']
+            with open(os.path.join(book_dir, f'{title}{ext}'), 'r', encoding='utf-8') as f:
+                abook = json.load(f)
+        else:
             abook = {}
         bookInfo = json.dumps(abook, ensure_ascii=False, indent=4)
 
+        if self.flags['introduction'] is False:
+            print('no introduction to gen videoStructure for {title}')
+            return False
+        
         ext = self.exts['introduction']
         with open(os.path.join(book_dir, f'{title}{ext}'), 'r', encoding='utf-8') as f:
             intro_txt = f.read()
-        if len(intro_txt) == 0:
-            print('failed to read introduction')
-            return False
+        
         query = tmpl.format(std_txt=intro_txt, book_info=bookInfo)
         asw = await self.llm.ask_llm(query, '')
         result = self.ans_extr.output_extr('video_structure1', asw)
@@ -191,7 +207,10 @@ class BookSummary:
     
     async def gen_visualDesc(self, title, book_dir):
         print(f"gen_visualDesc for {title}")
-
+        if self.flags['videoStructure'] is False:
+            print(f"Failed to generate visual desc for {title}")
+            return False
+        
         tmpl = self.prompter.tasks['visual_desc1']
         ext = self.exts['videoStructure']
         with open(os.path.join(book_dir, f'{title}{ext}'), 'r', encoding='utf-8') as f:
@@ -202,18 +221,18 @@ class BookSummary:
             print(f"sec: {sec['section']}")
             count = sec['duration']*6
             vidInfo = json.dumps(sec, ensure_ascii=False, indent=4)
-            query = tmpl.format(vid_stru=vidInfo,count=count)
-            with open('temp_query.txt', 'w', encoding='utf-8') as f:
-                f.write(query)
+            query = tmpl.format(vid_stru=vidInfo, count=count)
+            # with open('temp_query.txt', 'w', encoding='utf-8') as f:
+            #     f.write(query)
             asw = await self.llm.ask_llm(query, '')
             result = self.ans_extr.output_extr('visual_desc1', asw)
             if result['status'] == 'failed':
                 print(f"Failed to generate visual desc for {title}")
-                continue
+                return False
+            
             result = result['msg']
             shots = [{'section': sec['section'], **shot} for shot in result['shots']]
             allDesc.extend(shots)
-            
 
         for i, item in enumerate(allDesc):
             item['shot_number'] = i+1
@@ -227,8 +246,8 @@ class BookSummary:
         
         vidInfo = json.dumps({'shots': newDesc}, ensure_ascii=False, indent=4)
         query = tmpl.format(img_desc=vidInfo)
-        with open('temp_query.txt', 'w', encoding='utf-8') as f:
-            f.write(query)
+        # with open('temp_query.txt', 'w', encoding='utf-8') as f:
+        #     f.write(query)
 
         asw = await self.llm.ask_llm(query, '')
         result = self.ans_extr.output_extr('reused_image', asw)
@@ -236,12 +255,9 @@ class BookSummary:
             print(f"Failed to generate video structure for {title}")
             return False
         result = result['msg']
-        with open('temp_result.txt', 'w', encoding='utf-8') as f:
-            f.write(json.dumps(result, ensure_ascii=False, indent=4))
-
+        
         for group in result['reused_images']:
             reused = group['shots']
-            print(f"reused: {reused}")
             reused.sort()
             baseId = reused[0]
             for sId in reused[1:]:
